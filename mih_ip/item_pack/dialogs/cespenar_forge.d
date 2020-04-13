f96147118daaa18fd741af5bfed25b987db42b93
Angel <angel@pearlgates.net>
2020-04-13 16:00:51 +0200

    Nine new items!

---
+++ mih_ip/item_pack/dialogs/cespenar_forge.d
@@ -0,0 +1,37 @@
+EXTEND_BOTTOM "botsmith" 4
+  IF ~PartyHasItem("mh#comp6")~
+  THEN
+    GOTO mh#PartyHasBlackDragonScale
+END
+
+
+APPEND "botsmith"
+
+IF ~~
+BEGIN mh#PartyHasBlackDragonScale
+  SAY #66903
+  = @0
+  IF ~PartyGoldLT(5000)~
+    REPLY #66908
+    GOTO 10
+  IF ~PartyGoldGT(4999)~
+    REPLY #66909
+    DO ~SetGlobal("mh#CespenarForgeItem", "GLOBAL", 1)
+        SetGlobal("ImpForgeStuff", "GLOBAL", 1)
+	TakePartyGold(5000)
+	DestroyGold(5000)~
+    GOTO 11
+  IF ~~
+    REPLY #66913
+    GOTO mh#NotInterestedBlackDragonScale
+END
+
+IF ~~
+BEGIN mh#NotInterestedBlackDragonScale
+  SAY @1
+  COPY_TRANS "botsmith" 4
+END
+
+END	// APPEND
+
+
---
+++ mih_ip/item_pack/dialogs/cromwell_forge.d
@@ -0,0 +1,45 @@
+EXTEND_BOTTOM "wsmith01" 13
+  IF ~PartyHasItem("mh#comp6")~
+  THEN
+    GOTO mh#PartyHasBlackDragonScale
+END
+
+
+APPEND "wsmith01"
+
+IF ~~
+BEGIN mh#PartyHasBlackDragonScale
+  SAY #59709
+  = @0
+  IF ~~
+    REPLY #59717
+    GOTO mh#InterestedBlackDragonScale
+  IF ~~
+    REPLY #59718
+    GOTO mh#NotInterestedBlackDragonScale
+END
+
+IF ~~
+BEGIN mh#NotInterestedBlackDragonScale
+  SAY #59723
+  COPY_TRANS "wsmith01" 13
+END
+
+IF ~~
+BEGIN mh#InterestedBlackDragonScale
+  SAY #59771
+  IF ~PartyGoldGT(4999)~
+    REPLY #59775
+    DO ~SetGlobal("mh#CromwellForgeItem", "GLOBAL", 1)
+        SetGlobal("ForgeStuff", "GLOBAL", 1)
+	TakePartyGold(5000)
+	DestroyGold(5000)~
+    GOTO 56
+  IF ~~
+    REPLY #59718
+    GOTO mh#NotInterestedBlackDragonScale
+END
+
+END	// APPEND
+
+
