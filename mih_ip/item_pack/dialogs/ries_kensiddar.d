afe1b851d9b20b0c59c488e0eb9f7c5babf65f2b
Angel <angel@pearlgates.net>
2022-01-20 14:11:27 +0100

    Lore-friendly Item Pack

---
+++ mih_ip/item_pack/dialogs/ries_kensiddar.d
@@ -0,0 +1,56 @@
+APPEND "%tutu_var%innkn2"
+
+IF WEIGHT #-1 ~PartyHasItem("misc01") Global("mh#RiesWinterWolfHide", "GLOBAL", 0)~
+BEGIN hide_offer
+  SAY @0
+  IF ~~
+    REPLY @1
+    GOTO hide_requirements
+  IF ~~
+    REPLY @2
+    DO ~SetGlobal("mh#RiesWinterWolfHide", "GLOBAL", 1)~
+    UNSOLVED_JOURNAL @3
+    GOTO 0
+END
+
+IF ~~
+BEGIN hide_requirements
+  SAY @4
+  IF ~NumItemsParty("misc01", 2) PartyGoldGT(699)~
+    REPLY @5
+    GOTO hide_crafting
+  IF ~~
+    REPLY @6
+    DO ~SetGlobal("mh#RiesWinterWolfHide", "GLOBAL", 1)~
+    UNSOLVED_JOURNAL @3
+    GOTO 0
+END
+
+IF ~~
+BEGIN hide_crafting
+  SAY @7
+  IF ~~
+    DO ~SetGlobal("mh#RiesWinterWolfHide", "GLOBAL", 2) SetGlobalTimer("mh#RiesWinterWolfHideWait", "GLOBAL", TWO_DAYS) TakePartyItemNum("misc01", 3) DestroyItem("misc01") DestroyItem("misc01") DestroyItem("misc01") TakePartyGold(700)~
+    UNSOLVED_JOURNAL @8
+    EXIT
+END
+
+IF ~~
+BEGIN hide_finished
+  SAY @9
+  IF ~~
+    DO ~SetGlobal("mh#RiesWinterWolfHide", "GLOBAL", 3) GiveItemCreate("mh#hide1", LastTalkedToBy(Myself), 0, 0, 0)~
+    SOLVED_JOURNAL @10
+    EXIT
+END
+
+END	// APPEND "%tutu_var%innkn2"
+
+
+EXTEND_TOP "%tutu_var%innkn2" 0
+  IF ~Global("mh#RiesWinterWolfHide", "GLOBAL", 2) GlobalTimerExpired("mh#RiesWinterWolfHideWait", "GLOBAL")~
+    REPLY @11
+    GOTO hide_finished
+END
+
+
